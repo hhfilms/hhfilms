9943169a043db389061ed87ac63448e6022d46a3
Eric L <eric.lopez@nurocor.com>
2024-12-23 22:12:59 -0600

    move socials

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono, Assistant, Work_Sans} from "next/font/google";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import Socials from "../components/Socials";
+
 import "./globals.scss";
 
 const geistSans = Geist({
@@ -44,6 +46,8 @@ export default function RootLayout({
             <main className="flex-grow overflow-y-auto pt-36">{children}</main>
           </AppRouterCacheProvider>
           <Footer />
+          {/* Socials */}
+          <Socials />
         </div>
       </body>
     </html>
