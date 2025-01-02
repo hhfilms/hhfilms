ee5c4424b3329e9682196724a8182aa07da8bb4a
Eric L <eric.lopez@nurocor.com>
2025-01-02 01:41:43 -0600

    update privacy-policy

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="bg-midnight text-white py-4 text-center">
       <p>© {new Date().getFullYear()} Heart & Hustle Films. All rights reserved.</p>
-      <Link href="/privacy">Privacy</Link>
+      <Link href="/privacy-policy">Privacy</Link>
     </footer>
   );
 };
