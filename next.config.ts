c00ed491a9fb3adbb6f0c84ab757c3235bcf7e34
Eric L <eric.lopez@nurocor.com>
2025-01-03 00:57:42 -0600

    Add CSP to next config

--- next.config.ts
+++ next.config.ts
@@ -23,7 +23,7 @@ const nextConfig: NextConfig = {
               style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
               font-src 'self' https://fonts.gstatic.com data:;
               img-src 'self' https://scontent-dfw5-1.cdninstagram.com https://hhf-assets.s3.us-east-2.amazonaws.com https://hhf-assets.s3.amazonaws.com data:;
-              media-src 'self' https://hhf-assets.s3.us-east-2.amazonaws.com https://scontent-dfw5-1.cdninstagram.com;
+              media-src 'self' https://hhf-assets.s3.us-east-2.amazonaws.com https://scontent-dfw5-1.cdninstagram.com https://scontent-iad3-2.cdninstagram.com;
               script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.instagram.com;
               connect-src 'self' https://hhf-assets.s3.us-east-2.amazonaws.com;
             `
