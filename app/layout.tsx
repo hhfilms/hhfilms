02e457a8ce997ce3e844e471f38eaded42af9509
Eric L <eric.lopez@nurocor.com>
2025-01-03 15:40:16 -0600

    update meta and og image url

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,37 @@
 import {AppRouterCacheProvider} from "@mui/material-nextjs/v15-appRouter";
 import type {Metadata} from "next";
 import {Providers} from "./providers";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import Socials from "../components/Socials";
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+import Socials from "@/components/Socials";
 
 import "./globals.scss";
 
 export const metadata: Metadata = {
-  title: "heart & hustle films",
-  description: "sports videography | event coverage | cinematic storytelling",
+  title: "Heart & Hustle Films",
+  description: "sports videography | event coverage | business promos | cinematic storytelling",
+  openGraph: {
+    title: "Heart & Hustle Films",
+    description: "sports videography | event coverage | business promos | cinematic storytelling",
+    url: "https://hearthustlefilms.com",
+    siteName: "Heart & Hustle Films",
+    images: [
+      {
+        url: "https://hhf-assets.s3.us-east-2.amazonaws.com/ogimage.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Heart & Hustle Films Hero Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Heart & Hustle Films",
+    description: "sports videography | event coverage | business promos | cinematic storytelling",
+    images: ["https://hhf-assets.s3.us-east-2.amazonaws.com/ogimage.jpg"],
+  },
 };
 
 export default function RootLayout({
