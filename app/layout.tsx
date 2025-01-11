4595506491769f3439e1fe076a5f59054edab05d
Eric L <hearthustlefilms@gmail.com>
2025-01-11 13:20:14 -0600

    fix lint errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import type {Metadata} from "next";
 import {Providers} from "./providers";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
-import {CssBaseline} from "@mui/material";
 
 import "./globals.scss";
 
